95148e43384b062ea7c81a6d89080c84692b3b07
Dominik Szabo <szabovszky5@gmail.com>
2025-06-13 21:07:31 +0200

    Extracted Playground from GamePane

--- src/main/java/szabo/game/GamePane.java
+++ src/main/java/szabo/game/GamePane.java
@@ -11,7 +11,6 @@ import javafx.scene.shape.Rectangle;
 
 public class GamePane extends BorderPane {
 
-    private static final double LINE_POSITION = 0.2;  // % of playground height
     private static final String SCORE = "Score: ";
     private static final String HIGH_SCORE = "High Score: ";
 
@@ -19,6 +18,7 @@ public class GamePane extends BorderPane {
     private final Leaderboard leaderboard;
     private final Label scoreLabel = new Label();
     private final Label highScoreLabel = new Label();
+    private final Playground playground = new Playground();
 
     private boolean beingDragged = false;
 
@@ -50,56 +50,7 @@ public class GamePane extends BorderPane {
 
         // ------------------------------------------------
 
-        var playgroundPane = new Pane();
-        var container = new StackPane(playgroundPane);
-        setCenter(container);
-        playgroundPane.setBackground(Background.fill(Color.web("rgba(0, 0, 0, 0.4)")));
-        playgroundPane.setBorder(new Border(new BorderStroke(
-                Color.WHITE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,
-                new BorderWidths(
-                        0, GameHandler.WALL_THICKNESS, GameHandler.WALL_THICKNESS, GameHandler.WALL_THICKNESS,
-                        false, true, true, true
-                )
-        )));
-
-        Line line = new Line(0, 20, playgroundPane.getWidth(), 20);
-        line.setStroke(Color.WHITE);
-        line.endXProperty().bind(playgroundPane.widthProperty());
-        line.startYProperty().bind(playgroundPane.heightProperty().multiply(LINE_POSITION));
-        line.endYProperty().bind(playgroundPane.heightProperty().multiply(LINE_POSITION));
-        playgroundPane.getChildren().add(line);
-
-        playgroundPane.heightProperty().addListener(
-                e -> System.out.println("Width to Height: " + playgroundPane.getWidth() / playgroundPane.getHeight())
-        );
-        playgroundPane.widthProperty().addListener(
-                e -> System.out.println("Width to Height: " + playgroundPane.getWidth() / playgroundPane.getHeight())
-        );
-
-        playgroundPane.maxWidthProperty().bind(Bindings.createDoubleBinding(
-                () -> Math.min(container.getWidth(), container.getHeight() * GameHandler.PLAYGROUND_RATIO),
-                container.widthProperty(),  container.heightProperty()
-        ));
-        playgroundPane.maxHeightProperty().bind(Bindings.createDoubleBinding(
-                () -> Math.min(container.getHeight(), container.getWidth() / GameHandler. PLAYGROUND_RATIO),
-                container.heightProperty(),  container.widthProperty()
-        ));
-
-        var rec = new Rectangle(40, 40, 30, 30);
-        rec.setFill(Color.GREEN);
-        playgroundPane.getChildren().add(rec);
-
-        playgroundPane.setOnMousePressed(e -> {
-            beingDragged = rec.intersects(e.getX(), e.getY(), 1, 1);
-            System.out.println(beingDragged);
-        });
-        playgroundPane.setOnMouseReleased(e -> beingDragged = false);
-        playgroundPane.setOnMouseDragged(e -> {
-            if (beingDragged) {
-                rec.setX(Math.max(0, Math.min(e.getX(), playgroundPane.getWidth() - rec.getWidth())));
-                rec.setY(Math.max(0, Math.min(e.getY(), playgroundPane.getHeight() - rec.getHeight())));
-            }
-        });
+        setCenter(playground);
 
     }
 
---
+++ src/main/java/szabo/game/Playground.java
@@ -0,0 +1,68 @@
+package szabo.game;
+
+import javafx.beans.binding.Bindings;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+
+public class Playground extends StackPane {
+    private static final double LINE_POSITION = 0.2;  // % of playground height
+
+    private final Pane playgroundPane = new Pane();
+    private boolean beingDragged = false;
+
+
+    public Playground() {
+        super();
+        getChildren().add(playgroundPane);
+        playgroundPane.setBackground(Background.fill(Color.web("rgba(0, 0, 0, 0.4)")));
+        playgroundPane.setBorder(new Border(new BorderStroke(
+                Color.WHITE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY,
+                new BorderWidths(
+                        0, GameHandler.WALL_THICKNESS, GameHandler.WALL_THICKNESS, GameHandler.WALL_THICKNESS,
+                        false, true, true, true
+                )
+        )));
+
+        Line line = new Line(0, 20, playgroundPane.getWidth(), 20);
+        line.setStroke(Color.WHITE);
+        line.endXProperty().bind(playgroundPane.widthProperty());
+        line.startYProperty().bind(playgroundPane.heightProperty().multiply(LINE_POSITION));
+        line.endYProperty().bind(playgroundPane.heightProperty().multiply(LINE_POSITION));
+        playgroundPane.getChildren().add(line);
+
+        playgroundPane.heightProperty().addListener(
+                e -> System.out.println("Width to Height: " + playgroundPane.getWidth() / playgroundPane.getHeight())
+        );
+        playgroundPane.widthProperty().addListener(
+                e -> System.out.println("Width to Height: " + playgroundPane.getWidth() / playgroundPane.getHeight())
+        );
+
+        playgroundPane.maxWidthProperty().bind(Bindings.createDoubleBinding(
+                () -> Math.min(getWidth(), getHeight() * GameHandler.PLAYGROUND_RATIO),
+                widthProperty(),  heightProperty()
+        ));
+        playgroundPane.maxHeightProperty().bind(Bindings.createDoubleBinding(
+                () -> Math.min(getHeight(), getWidth() / GameHandler. PLAYGROUND_RATIO),
+                heightProperty(),  widthProperty()
+        ));
+
+        var rec = new Rectangle(40, 40, 30, 30);
+        rec.setFill(Color.GREEN);
+        playgroundPane.getChildren().add(rec);
+
+        playgroundPane.setOnMousePressed(e -> {
+            beingDragged = rec.intersects(e.getX(), e.getY(), 1, 1);
+            System.out.println(beingDragged);
+        });
+        playgroundPane.setOnMouseReleased(e -> beingDragged = false);
+        playgroundPane.setOnMouseDragged(e -> {
+            if (beingDragged) {
+                rec.setX(Math.max(0, Math.min(e.getX(), playgroundPane.getWidth() - rec.getWidth())));
+                rec.setY(Math.max(0, Math.min(e.getY(), playgroundPane.getHeight() - rec.getHeight())));
+            }
+        });
+    }
+
+}
