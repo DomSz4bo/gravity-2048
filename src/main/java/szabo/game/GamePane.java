66cc822f96ccd538b439c7140ba8847b250c3c57
Dominik Szabo <szabovszky5@gmail.com>
2025-06-17 22:58:26 +0200

    Label size binding

--- src/main/java/szabo/game/GamePane.java
+++ src/main/java/szabo/game/GamePane.java
@@ -1,9 +1,16 @@
 package szabo.game;
 
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.*;
+import javafx.scene.control.Labeled;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
 
 import java.util.Optional;
 
@@ -14,30 +21,46 @@ public class GamePane extends BorderPane {
     private final Playground playground = new Playground();
 
     public GamePane(Runnable onExit) {
-        scoreLabel.setStyle("-fx-font-size: 25");
+//        scoreLabel.setStyle("-fx-font-size: 25");
         scoreLabel.setMaxWidth(Double.MAX_VALUE);
         scoreLabel.setAlignment(Pos.CENTER);
         HBox.setHgrow(scoreLabel, Priority.ALWAYS);
 
-        scoreToBeatLabel.setStyle("-fx-font-size: 25");
-
         var exitButton = new Button("Back to Menu");
         exitButton.setStyle("-fx-font-size: 25");
         exitButton.setOnAction(event -> onExit.run());
+        exitButton.paddingProperty().bind(Bindings.createObjectBinding(
+                () -> {
+                    double fontSize = exitButton.getFont().getSize();
+                    double vPad = fontSize / 6;
+                    double hPad = fontSize * 0.8;
+                    return new Insets(vPad, hPad, vPad, hPad);
+                }, exitButton.fontProperty()
+        ));
 
         var scorePane = new HBox(scoreToBeatLabel, scoreLabel, exitButton);
         scorePane.setAlignment(Pos.CENTER);
-        scorePane.prefHeightProperty().bind(heightProperty().divide(16));
+        scorePane.paddingProperty().bind(Bindings.createObjectBinding(
+                () -> {
+                    double padding = widthProperty().get()/50;
+                    return new Insets(padding/4, padding, padding/4, padding);
+                }, widthProperty()
+        ));
 
         setCenter(playground);
         setTop(scorePane);
 
+        Platform.runLater(() -> {
+            bindFontSize(scoreLabel);
+            bindFontSize(scoreToBeatLabel);
+            bindFontSize(exitButton);
+        });
     }
 
     public void paint(GameState gameState, Leaderboard leaderboard) {
         Optional<Integer> scoreToBeat = findScoreToBeat(gameState.score(), leaderboard);
         String scoreToBeatText = scoreToBeat.map(i -> "Score to beat: " + i)
-                                            .orElse("New High Score!");
+                .orElse("New High Score!");
         scoreToBeatLabel.setText(scoreToBeatText);
         scoreLabel.setText("Score: " + gameState.score());
         playground.paintBlocks(gameState.getAllBlocks());
@@ -51,6 +74,20 @@ public class GamePane extends BorderPane {
 
     public Playground getPlayground() {
         return playground;
+    }
+
+    private void bindFontSize(Labeled element) {
+        var currentFont = element.getFont();
+        element.fontProperty().bind(Bindings.createObjectBinding(
+                () -> {
+                    double widthBound = getWidth() * 0.025;
+                    double heightBound = getHeight() * 0.035;
+                    return Font.font(
+                            currentFont.getFamily(),
+                            Math.max(12, Math.min(widthBound, heightBound))
+                    );
+                }, widthProperty(), heightProperty()
+        ));
     }
 
 }
