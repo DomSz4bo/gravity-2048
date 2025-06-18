8c732889375a943e6be652224f7b2e62ef39d000
Dominik Szabo <szabovszky5@gmail.com>
2025-06-19 00:18:02 +0200

    module requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,6 @@ module szabo.game {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.dyn4j;
-    requires java.sql;
-    requires java.desktop;
-
 
     opens szabo.game to javafx.fxml;
     exports szabo.game;
